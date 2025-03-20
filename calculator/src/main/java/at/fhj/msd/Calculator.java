91d49ef4f97c95d87539e137d2ab465847f7d68d
marchatuzaeva <marcha.tuzaeva@edu.fh-joanneum.at>
2025-03-20 21:24:02 +0100

    Add updated Calculator class with modifications

--- calculator/src/main/java/at/fhj/msd/Calculator.java
+++ calculator/src/main/java/at/fhj/msd/Calculator.java
@@ -7,11 +7,5 @@ public class Calculator {
       return number1 - number2;
     }
 
-    public double multiply(double number1, double number2) {
-      return number1 * number2;
-    }
-
-    public double divide(double number1, double number2) {
-      return number1 / number2;
-    }
+    public double multiply(double number1, double number2git
 }
